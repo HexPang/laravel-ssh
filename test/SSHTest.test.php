cf52ae27d1e452f0789ff9830f22172a6a95d3fd
HexPang <hexpang@jabstruse.com>
2016-08-24 17:36:12 +0800

    add CircleCI config file,add phpunit config file and composer.

---
+++ test/SSHTest.test.php
@@ -0,0 +1,13 @@
+<?php
+namespace hexpang\Services;
+
+
+class SSHTest extends \PHPUnit_Framework_TestCase {
+  public function testInstance(){
+    $this->assertTrue(true);
+    // $service = new SSHService();
+    // $this->assertInstanceOf("\hexpang\Services\SSHService",$service);
+  }
+}
+
+?>
